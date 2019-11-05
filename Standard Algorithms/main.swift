807fecd2947142d0072ca717b46c93d30cbe1bb3
Billy Manners <manners.w@etoncollege.org.uk>
2019-11-05 11:12:31 +0000

    Initial check

---
+++ Standard Algorithms/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Standard Algorithms
+//
+//  Created by Manners, William (Coll) on 05/11/2019.
+//  Copyright © 2019 Manners, William (Coll). All rights reserved.
+//
+
+import Foundation
+let name = ("whatever")
+print("Hello, World!")
+print(name)
