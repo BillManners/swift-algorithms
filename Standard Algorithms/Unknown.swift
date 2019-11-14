5fa0bc282d416eed348a4e5f474d2af14e49b13d
Billy Manners <manners.w@etoncollege.org.uk>
2019-11-14 12:06:37 +0000

    Mid task to send to PMD

---
+++ Standard Algorithms/Unknown.swift
@@ -0,0 +1,15 @@
+//
+//  Unknown.swift
+//  Standard Algorithms
+//
+//  Created by Manners, William (Coll) on 14/11/2019.
+//  Copyright © 2019 Manners, William (Coll). All rights reserved.
+//
+
+import Foundation
+
+class Unknown (){
+    func firstAndLast (input : [Int]) -> Int {
+        if array.count
+    }
+}
--- Standard Algorithmstests/Search Test.swift
+++ Standard Algorithmstests/Search Test.swift
@@ -6,21 +6,21 @@
 //  Copyright © 2019 Manners, William (Coll). All rights reserved.
 //
 
-import Foundation
-import XCTest
-
-class Search_Test: XCTestCase {
-    
-    func testLinearSearch() {
-        //arrange
-        let searches = Searches()
-        let testCases = [(array: [5,2,6,27,8,256,2,8], search_term: 5, expected: 0),
-                         (array: [2,75,84,9,4,2], search_term: 84, expected: 2)]
-        //act
-        //assert
-        for testCase in testCases {
-            let actual = searches.linearSearch(mainList: testCases.array , searchTerm : testCases.searchTerm)
-            XCTAssertEqual(actual, testCase.expected)
-        }
+//import Foundation
+//import XCTest
+//
+//class Search_Test: XCTestCase {
+//
+//    func testLinearSearch() {
+//        //arrange
+//        //let searches = Searches()
+//        let testCases = [(array: [5,2,6,27,8,256,2,8], search_term: 5, expected: 0),
+//                         (array: [2,75,84,9,4,2], search_term: 84, expected: 2)]
+//        //act
+//        //assert
+//        for testCase in testCases {
+//            let actual = searches.linearSearch(mainList: testCases.array , searchTerm : testCases.searchTerm)
+//            XCTAssertEqual(actual, testCase.expected)
+//        }
     }
 }
